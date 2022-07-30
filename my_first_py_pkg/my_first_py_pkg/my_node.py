3e0b682e43bcee5b931dd15ae826bbbeb5e36b1d
Manish Nayak <mkn123abc@gmail.com>
2022-07-30 20:38:34 +0530

    Created my first Python Package

---
+++ my_first_py_pkg/my_first_py_pkg/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from my_first_py_pkg.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ my_first_py_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'my_first_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='qygxamu',
+    maintainer_email='mkn123abc@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = my_first_py_pkg.my_node:main'
+        ],
+    },
+)
